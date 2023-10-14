0effb28bcfafb1eca1a66527b2dca6d5321b798c
w-vinhatto <vvinhatto@hotmail.com>
2023-10-14 19:37:43 -0300

    comit para ajuste do numeros do imput

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function generateNumber(){
 
-    const min = Math.ceil(document.querySelector("#min").value)
-    const max = Math.floor(document.querySelector("#max").value)
+    function generateNumber(){
 
-    const vencedor = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    alert(vencedor, "parabéns ao vencedor")
+        const min = Math.ceil(document.querySelector("#min").value)
+        const max = Math.floor(document.querySelector("#max").value)
+    
+        if ( min >= max ) {
+            alert("o numero de entrada precisa ser menor que número total")
+        } else{
+        const vencedor = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+        alert(vencedor, "parabéns ao vencedor")
+    }
 }
